e7680c25ae8422460b51fe850929cf3b135b94d5
emmitt <emmittchoi2002@gmail.com>
2025-07-07 16:30:07 -0700

    Add testing for Project

--- src/index.js
+++ src/index.js
@@ -1,73 +1,36 @@
-function Card(title, desc, dueDate, priority, parentId, notes="", checklist=[], parentId) {
+function Card(title, desc, dueDate, priority, notes="", checklist=[]) {
     let _title = title; // str
     let _desc = desc; // str
     let _dueDate = dueDate; // Date
     let _priority = priority; // integer from 1-5 (1 is least important, 5 most
-    let _parentId = parentId; // uuid of parent Project
     let _notes = notes; // str
     let _checklist = checklist; // TODO implement me
 
     let complete = false;
     const id = crypto.randomUUID();
+    let parentId = null; // uuid of parent
 
     const getCard = () => {}; // TODO
     const getId = () => id;
+    const getParentId = () => parentId;
+    const setParentId = (newParentId) => { parentId = newParentId }; 
     
     function editCard(newTitle=_title, newDesc=_desc, newDueDate=_dueDate,
-     newPriority=_priority, newParentId=_parentId, newNotes=_notes, newChecklist=_checklist) {
+     newPriority=_priority, newNotes=_notes, newChecklist=_checklist) {
         _title=newTitle;
         _desc=newDesc;
         _dueDate=newDueDate;
         _priority=newPriority;
-        _parentId=newParentId;
         _notes=newNotes;
         _checklist=newChecklist;
     }
 
     const toggleComplete = () => {complete = !complete};
-    const printCard = () => { console.log(card) };
+    const printCard = () => { console.log(`Title: ${_title}, Desc: ${_desc}, DueDate: ${_dueDate}, 
+                            Priority: ${_priority}, Notes: ${_notes}, Checklist: ${_checklist}`) };
 
-    return { getCard, getId, editCard, toggleComplete, printCard };
+    return { getCard, getId, editCard, toggleComplete, printCard, getParentId, setParentId };
 }
-
-/*
-function Deck(name) {
-    // named collection of Cards
-    const deck = [];
-
-    let _name = name;
-    const id = crypto.randomUUID();
-
-    const addCard = (card) => {deck.push(card)};
-    const removeCard = (uuid) => {
-        for (let i = 0; i < deck.length; i++) {
-            const card = deck[i]
-        if (card.id === uuid) {
-            deck.splice(i, 1);
-            break;
-        }
-    }
-    };
-    const getCard = (uuid) => {
-        for (const card of deck) {
-            if (card.id === uuid) {
-                return card
-            }
-        }
-        return null
-    }; // get card with uuid ow returns null
-
-    const printDeck = () => { deck.forEach(card => {card.printCard()}) };
-    const getName = () => _name;
-    const editName = (newName) => { _name=newName };
-    const getId = () => id;
-
-    return { addCard, removeCard, getCard, printDeck, getName, editName, getId };
-}
-*/
-
-// TODO make Deck into abstract Collection function, then compose specifics for Project (collection of Card) and
-// the whole TODO list (collection of Projects)
 
 
 function Deck() {
@@ -113,3 +76,14 @@ function Project(name) {
 
     return { getName, editName, getId, addCard, removeCard, getCard, retrieveAllCards };
 }
+
+
+let card1 = Card("I'm a card", "none", "6/13", "5");
+let card2 = Card("I'm also a card", "none", "2/13", "1");
+
+let testProject = Project("default");
+testProject.addCard(card1);
+testProject.addCard(card2);
+let allCards = testProject.retrieveAllCards();
+allCards[0].printCard();
+allCards[1].printCard();
